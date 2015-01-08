04c92f0fd08a773dd466291f7513ca8c585cb75c
Thomas Schmidt <schmidt@netaction.de>
2015-01-09 00:17:52 +0100

    First blood

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var clicked = false, moved = false, clickX, clickY;
+
+$("#layer").on({
+	'mousemove': function(e) {
+		moved = true;
+		clicked && updateScrollPos(e);
+	},
+	'mousedown': function(e) {
+		clicked = true;
+		moved = false;
+		clickX = e.pageX;
+		clickY = e.pageY;
+	},
+	'mouseleave': function(e) {
+		clicked = false;
+		$('body').removeClass('scrolling');
+	},
+	'mouseup': function(e) {
+		clicked = false;
+		if (!moved) {
+			// not moved
+			$("body").toggleClass("overview");
+		}
+		$('body').removeClass('scrolling');
+	}
+});
+
+var updateScrollPos = function(e) {
+	$('body').addClass('scrolling');
+	$(window)
+		.scrollLeft($(window).scrollLeft() + (clickX - e.pageX))
+		.scrollTop($(window).scrollTop() + (clickY - e.pageY));
+}
